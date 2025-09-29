9fa801af0cdcbfe280fd348fdd0a93a2a783b9b8
Moon-Sangho <skym887@gmail.com>
2025-09-29 21:44:33 +0900

    chore: css 및 정적자원(이미지, 폰트) webpack 설정 추가

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,13 @@
+import './styles.css';
+import reactSvg from './assets/svgs/react.svg';
+
 const App = () => {
-  return <div>Hello Webpack!</div>;
+  return (
+    <div className="container">
+      <h1>Hello, React with TypeScript and Webpack!</h1>
+      <img src={reactSvg} alt="Sample" />
+    </div>
+  );
 };
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -50,10 +50,32 @@ const config = {
         ],
         exclude: /node_modules/,
       },
+      /**
+       * CSS 처리
+       * 배열의 마지막 loader부터 순서대로 적용됨
+       */
       {
-        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        test: /\.css$/,
+        use: [
+          'style-loader', //  CSS를 <style> 태그로 주입
+          'css-loader', //  CSS를 JavaScript 모듈로 변환
+        ],
+      },
+      /**
+       * 정적 리소스 (이미지, 폰트 등) 처리
+       * asset/resource: 이미지를 독립적인 파일로 생성하여 파일 경로를 제공. 대형 이미지에 적합.
+       * asset/inline: 이미지를 텍스트 형태(Base64)로 변환하여 JavaScript 파일에 직접 포함. 소형 이미지에 적합.
+       * asset: 파일 크기에 따라 resource 또는 inline 자동 선택(기본값: 8kb)
+       */
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|webp)$/i, // 이미지 파일 확장자
         type: 'asset',
       },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i, // 폰트 파일 대상
+        type: 'asset/resource', // 폰트는 이미지보다 훨씬 용량이 크고, 자주 바뀌지 않는 정적 자원. 브라우저가 캐싱하여 네트워크 비용을 줄일 수 있도록 항상 별도 파일로 제공
+      },
+
       // Add your rules for custom modules here
       // Learn more about loaders from https://webpack.js.org/loaders/
     ],
