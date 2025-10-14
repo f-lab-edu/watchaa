51908091e36abab5f979cd38d76b803076e6e642
Moon-Sangho <skym887@gmail.com>
2025-10-14 17:44:20 +0900

    refactor: 메인 페이지 컴포넌트들의 loading ui 수정

--- src/features/movie/components/poster-carousel/loading.tsx
+++ src/features/movie/components/poster-carousel/loading.tsx
@@ -1,11 +1,13 @@
-import Spinner from '@/components/spinner';
-
 const PosterCarouselLoading = () => {
   return (
-    <div>
-      <p className="text-red-500">Loading posters...</p>
-      <Spinner />
-    </div>
+    <ul className="flex gap-3">
+      {[...Array(8)].map((_, index) => (
+        <li
+          key={index}
+          className="size-full animate-pulse bg-[var(--color-background30)] aspect-2/3 rounded-md mb-2"
+        />
+      ))}
+    </ul>
   );
 };
 
