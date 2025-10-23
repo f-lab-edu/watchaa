18382651e1503ffc8e65f88d846f48234097d1a0
Moon-Sangho <skym887@gmail.com>
2025-10-23 17:28:01 +0900

    refactor: 잘못 삽입된 svg 라인 제거

--- src/components/icons/plus.tsx
+++ src/components/icons/plus.tsx
@@ -1,20 +1,3 @@
-<svg
-  xmlns="http://www.w3.org/2000/svg"
-  width="24"
-  height="24"
-  fill="none"
-  viewBox="0 0 24 24"
-  aria-hidden="true"
-  className="IQ2ur"
->
-  <path
-    fill="currentColor"
-    fillRule="evenodd"
-    d="M12 2a.75.75 0 0 0-.75.75v8.5h-8.5a.75.75 0 0 0 0 1.5h8.5v8.5a.75.75 0 0 0 1.5 0v-8.5h8.5a.75.75 0 0 0 0-1.5h-8.5v-8.5A.75.75 0 0 0 12 2"
-    clipRule="evenodd"
-  />
-</svg>;
-
 const PlusIcon = (props: React.SVGProps<SVGSVGElement>) => (
   <svg {...props} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true">
     <path
